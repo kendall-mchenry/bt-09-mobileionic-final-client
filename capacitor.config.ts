ff7bb4e9430ebb2f2ec41fd3cd6516bea37c291c
Kendall McHenry <kendallexplores@gmail.com>
2022-10-15 21:13:22 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'final-task-client',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
